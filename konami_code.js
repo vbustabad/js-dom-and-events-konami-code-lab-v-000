a5d960026e5fabb80b841c051e89b72a49310fa6
Veronica Bustabad <vbustabad@gmail.com>
2018-09-16 01:00:29 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
+  let index = 0;
 
-  if (key === code[index]) {
-    index++;
+  document.body.addEventListener('keydown', function(e) {
 
-    if (index === code.length) {
-      alert("Hurray!");
+    const key = parseInt(e.detail || e.which);
 
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
+  });
 }
