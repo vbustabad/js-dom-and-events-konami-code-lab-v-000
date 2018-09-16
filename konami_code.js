c5d2f949bb8cfdeda8dd484b02dccb24afb589d8
Veronica Bustabad <vbustabad@gmail.com>
2018-09-16 00:57:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,18 +5,18 @@ function init() {
 
   document.body.addEventListener('keydown', function(e) {
 
-  const key = parseInt(e.detail || e.which);
+    const key = parseInt(e.detail || e.which);
 
-  if (key === code[index]) {
-    index++;
+    if (key === code[index]) {
+      index++;
 
-    if (index === code.length) {
-      alert("Hurray!");
+      if (index === code.length) {
+        alert("Hurray!");
 
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
   });
 }
